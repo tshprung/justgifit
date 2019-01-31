4e637a1c4ce7191987d667d3769ab527f968c567
tshprung <tshprung@gmail.com>
2019-01-31 08:06:31 +0200

    initialized new repo, after downloading from start.spring.io

---
+++ src/main/java/shprung/tal/com/justgifit/JustGifItApplication.java
@@ -0,0 +1,14 @@
+package shprung.tal.com.justgifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGifItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGifItApplication.class, args);
+	}
+
+}
+
